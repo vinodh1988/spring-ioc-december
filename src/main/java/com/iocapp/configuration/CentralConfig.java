e0ee20d8de92df1fc255cefc547f1d5d391fe434
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2025-12-15 17:21:07 +0530

    latest

--- src/main/java/com/iocapp/configuration/CentralConfig.java
+++ src/main/java/com/iocapp/configuration/CentralConfig.java
@@ -1,11 +1,27 @@
 package com.iocapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.iocapp.system.Activity;
+import com.iocapp.system.DigitalDocumenting;
+import com.iocapp.system.Documenting;
 
 @Configuration
 public class CentralConfig {
 
 	{
 		System.out.println("CentralConfig instantiated");
+	} // runs automatically everytime object is created
+	
+	@Bean(name="simpledoc")
+	public Activity document() {
+		return new Documenting();
+	}
+
+	@Bean(name="digidoc")
+	public Activity DigitalDocumenting() {
+		return new DigitalDocumenting();
 	}
 }
+

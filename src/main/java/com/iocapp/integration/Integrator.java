cc0d790920734af44d55bbcee465987fbb935e71
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2025-12-16 15:17:35 +0530

    latest

--- src/main/java/com/iocapp/integration/Integrator.java
+++ src/main/java/com/iocapp/integration/Integrator.java
@@ -1,20 +1,23 @@
 package com.iocapp.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.iocapp.system.ProcessX;
 
 public class Integrator {
   @Autowired
-  private ProcessX write; 
+  @Qualifier("read")
+  private ProcessX x; 
   @Autowired
-  private ProcessX read;
+  @Qualifier("write")
+  private ProcessX y;//Autowiring by name
   // this is only reference (null by default)
             //but spring ioc injects object if it finds anything appropriate
      //in our cass there is only implementation for ProcessX ie., ReadData
      //so ReadData singleton is injected
   public void doTask() {
-	  read.perform();
-	  write.perform();
+	  x.perform();
+	  y.perform();
   }
 }

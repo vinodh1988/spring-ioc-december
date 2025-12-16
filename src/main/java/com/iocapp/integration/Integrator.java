6174222aab3506bfa2aaa6b9400f8b15d6da395b
vinodhtrainer@gmail.com <vinodhtrainer@gmail.com>
2025-12-16 15:09:21 +0530

    lates

--- src/main/java/com/iocapp/integration/Integrator.java
+++ src/main/java/com/iocapp/integration/Integrator.java
@@ -6,11 +6,15 @@ import com.iocapp.system.ProcessX;
 
 public class Integrator {
   @Autowired
-  private ProcessX read; // this is only reference (null by default)
+  private ProcessX write; 
+  @Autowired
+  private ProcessX read;
+  // this is only reference (null by default)
             //but spring ioc injects object if it finds anything appropriate
      //in our cass there is only implementation for ProcessX ie., ReadData
      //so ReadData singleton is injected
   public void doTask() {
 	  read.perform();
+	  write.perform();
   }
 }
